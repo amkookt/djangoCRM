b54ed172378430000024379d2d3a0aa7b643f746
amkookt <amkookt3@gmail.com>
2021-01-26 14:16:32 -0800

    add models

---
+++ leads/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+
+
+class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('Youtube','Youtube'),
+        ('Google','Google'),
+        ('NewsLetter','NewsLetter'),
+    )
+
+
+
+    first_name = models.CharField(max_length=20)
+    last_name  = models.CharField(max_length=20)
+    age        = models.IntegerField(default=0)
+
+    phoned     = models.BooleanField(default=False)
+    source     = models.CharField(choices=SOURCE_CHOICES,max_length=100)
